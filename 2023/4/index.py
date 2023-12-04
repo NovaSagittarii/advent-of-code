234502299d1e5b0b72cc5d6e0bbc74f8a9a63856
NovaSagittarii <novasagittarii@gmail.com>
2023-12-03 22:11:44 -0700

    d4 p2 390
    yay dp

--- 2023/4/index.py
+++ 2023/4/index.py
@@ -1,14 +1,17 @@
 import sys
 lines = [line.strip() for line in sys.stdin]
 
-ans = 0
-for line in lines:
+for i, line in enumerate(lines):
 	card, content = line.split(': ')
 	win, num = [[int(x) for x in l.strip().split()] for l in content.split(' | ')]
 	pts = 0
 	for x in num:
 		if x in win:
-			if pts == 0: pts = 1
-			else: pts *= 2
-	ans += pts
-print(ans)
+			pts += 1
+	lines[i] = pts
+dp = [1 for i in lines]
+for i in range(len(dp)):
+	for j in range(lines[i]):
+		dp[i+j+1] += dp[i]
+# print(dp)
+print(sum(dp))
