db932e2a8da232b84769610d3acaf22ccd55868f
NovaSagittarii <novasagittarii@gmail.com>
2023-12-07 22:17:57 -0700

    d8 p2 430
    i did not realize the cycles would be so kind xD

--- 2023/8/index.py
+++ 2023/8/index.py
@@ -1,3 +1,4 @@
+import math
 import sys
 lines = [line.strip() for line in sys.stdin]
 arrayint = lambda arr: list(int(x) for x in arr)
@@ -10,9 +11,30 @@ for line in lines[2:]:
 	b, c = bc.split(", ")
 	adj[a] = (b, c)
 
-curr = 'AAA'
+curr = [curr for curr in adj.keys() if curr[-1] == 'A']
+
+hist = [[] for i in curr]
+his2 = [[] for i in curr]
+
 i = 0
-while curr != 'ZZZ':
-	curr = adj[curr][0 if steps[i%len(steps)] == 'L' else 1]
-	i += 1
+ok = False
+while not ok:
+    ok = True
+    for j in range(len(curr)):
+        curr[j] = adj[curr[j]][0 if steps[i%len(steps)] == 'L' else 1]
+    i += 1
+    for j, w in enumerate(curr):
+        if w[-1] != 'Z':
+            ok = False
+            continue
+        else:
+            if len(hist[j]): his2[j].append(i - hist[j][-1][0])
+            hist[j].append((i, w))
+    if i > 100000:
+        print(hist)
+        cycles = [x[0] for x in his2]
+        print(cycles)
+        print(math.lcm(*cycles))
+        
+        break
 print(i)
