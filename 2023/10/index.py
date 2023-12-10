ad628dfcb415456e3dbbf827ffdf36425983badf
NovaSagittarii <novasagittarii@gmail.com>
2023-12-09 23:30:35 -0700

    d10 p2 865
    i shouldn't have to tried to not expand the graph :pensive: :pray:

--- 2023/10/index.py
+++ 2023/10/index.py
@@ -1,12 +1,22 @@
 import math
 import sys
-lines = ['.'+line.strip()+'.' for line in sys.stdin]
+lines = [line.strip() for line in sys.stdin]
 arrayint = lambda arr: list(int(x) for x in arr)
 
+m = len(lines[0])
+lines = ['?'.join(list(line)) for line in lines]
+newlines = []
+for i, line in enumerate(lines):
+    if i > 0:
+        newlines.append(','.join(["?"] * m))
+    newlines.append(line)
+lines = newlines
+for i, line in enumerate(lines):
+    lines[i] = '.' + line + '.'
 
-m = len(lines[0]) + 1
+m = len(lines[0])
 lines = ['.'*m, *lines, '.'*m]
-n = len(lines) + 1
+n = len(lines)
 
 # for line in lines: print(line)
 
@@ -16,10 +26,10 @@ adj = [[[] for i in range(m)] for i in range(n)]
 
 for i, line in enumerate(lines):
     for j, char in enumerate(line):
-        up = char in '|LJ'
-        down = char in '|7F'
-        left = char in 'J7-'
-        right = char in 'LF-'
+        up = char in '|LJ?'
+        down = char in '|7F?'
+        left = char in 'J7-?'
+        right = char in 'LF-?'
         if down:
             adj[i][j].append((i+1, j))
             adj[i+1][j].append((i, j))
@@ -57,7 +67,7 @@ while q:
     nq = []
     for i, j in q:
         if vis[i][j]: continue
-        vis[i][j] = rounds
+        vis[i][j] = 1
         for ni, nj in adj[i][j]:
             if not vis[ni][nj]:
                 nq.append((ni, nj))
@@ -67,3 +77,29 @@ while q:
 # for lien in vis: print(lien)
 
 print(rounds)
+
+for row in vis:
+    # print(row)
+    # print("".join('#' if c == 1 else ' ' for c in row))
+    pass
+
+# Flood fill
+q = [(0, 0)]
+while q:
+    nq = []
+    for i, j in q:
+        if vis[i][j]: continue
+        vis[i][j] = 2
+        for di, dj in ((1,0), (-1,0), (0,1), (0,-1)):
+            ni = i+di
+            nj = j+dj
+            if 0 <= ni < n and 0 <= nj < m:
+                nq.append((ni, nj))
+    q = nq
+ans = 0
+for i, row in enumerate(vis):
+    print("".join(('!' if lines[i][j] not in ',?' else ' ',lines[i][j],lines[i][j])[x] for j,x in enumerate(row)))
+    for j, x in enumerate(row):
+        if x == 0 and lines[i][j] not in ',?':
+            ans += 1
+print(ans)
