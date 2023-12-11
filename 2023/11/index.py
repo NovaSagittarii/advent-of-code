973493c66e054ab860961f0079f074dedcf1c073
NovaSagittarii <novasagittarii@gmail.com>
2023-12-10 22:35:44 -0700

    d11 p2 1796
    when you do dijkstra APSP instead of doing prefixsum math, oops

--- 2023/11/index.py
+++ 2023/11/index.py
@@ -3,6 +3,9 @@ import sys
 lines = [line.strip() for line in sys.stdin]
 arrayint = lambda arr: list(int(x) for x in arr)
 
+n = len(lines)
+m = len(lines[0])
+
 emptyrow = [True for _ in lines]
 emptycol = [True for _ in lines[0]]
 for i, line in enumerate(lines):
@@ -11,28 +14,36 @@ for i, line in enumerate(lines):
             emptyrow[i] = False
             emptycol[j] = False
 
-newlines = []
-for i, line in enumerate(lines):
-    newline = ""
-    for j, c in enumerate(line):
-        newline += c
-        if emptycol[j]: newline += '.'
-    newlines.append(newline)
-    if emptyrow[i]: newlines.append('.' * len(newlines[0]))
-
-print("\n".join(newlines))
-lines = newlines
+cost = [[1 for l in line] for line in lines]
+K = 1000000
 
 coords = []
 for i, line in enumerate(lines):
     for j, c in enumerate(line):
         if c != '.':
             coords.append((i, j))
+        if emptyrow[i] or emptycol[j]:
+            cost[i][j] = K
 
 ans = 0
-for i, c in enumerate(coords):
-    x1, y1 = c
-    for d in coords[i+1:]:
-        x2, y2 = d
-        ans += abs(x1-x2) + abs(y1-y2)
+import heapq
+for si, sj in coords:
+    vis = [[False for _ in line] for line in lines]
+    pq = [(0, si, sj)]
+    while len(pq):
+        c, i, j = pq[0]
+        # print(pq[0])
+        heapq.heappop(pq)
+        # print(i, j)
+        if vis[i][j]: continue
+        vis[i][j] = True
+        if lines[i][j] != '.':
+            ans += c
+        for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni = i+di
+            nj = j+dj
+            if 0 <= ni < n and 0 <= nj < m:
+                if not vis[ni][nj]:
+                    heapq.heappush(pq, (c + cost[ni][nj], ni, nj))
+ans //= 2
 print(ans)
