19e6eab0e13dd43378f5dc65815d21ff11efa5b6
NovaSagittarii <novasagittarii@gmail.com>
2023-12-12 22:13:07 -0700

    d13 p1 171
    i dont remember where i had to do reflections before but im glad that came in handy

---
+++ 2023/13/index.py
@@ -0,0 +1,46 @@
+import math
+import sys
+lines = [line.strip() for line in sys.stdin]
+arrayint = lambda arr: list(int(x) for x in arr)
+
+grids = ("\n".join(lines)).split("\n\n")
+
+ans = 0
+for grid in grids:
+    grid = grid.split("\n")
+    n = len(grid)
+    m = len(grid[0])
+    w = -1
+    # vertical
+    for t in range(0, n-1):
+        k = t+0.5
+        ok = True
+        for i in range(t+1):
+            ni = int(k + (k-i))
+            for j in range(m):
+                if 0 <= ni < n:
+                    if grid[i][j] != grid[ni][j]:
+                        ok = False
+        if ok:
+            w = t+1
+            w *= 100
+            break
+    # horizontal
+    if w == -1:
+        for t in range(0, m-1):
+            k = t+0.5
+            ok = True
+            for j in range(t+1):
+                nj = int(k + (k-j))
+                for i in range(n):
+                    if 0 <= nj < m:
+                        if grid[i][j] != grid[i][nj]:
+                            ok = False
+            if ok:
+                w = t+1
+                break
+    # print('#', n, m)
+    # print(grid)
+    print(w)
+    ans += w
+print(ans)
