075db8a31c17d7c93c55bf535343c837188cddd6
NovaSagittarii <novasagittarii@gmail.com>
2023-12-15 23:56:03 -0700

    d16 p2 4956
    well that was pretty easy, a bit slow though :thinking:

--- 2023/16/index.py
+++ 2023/16/index.py
@@ -7,44 +7,56 @@ lines = [list(line) for line in lines]
 
 n = len(lines)
 m = len(lines[0])
-vis = [[[False,False,False,False] for w in line] for line in lines]
 DIRS = ((0, 1), (1, 0), (0, -1), (-1, 0))
-q = [(0, 0, 0)]
 ## \ >v , v>, 
 #  3
 #  ^
 #2< > 0
 #  v
 #  1
-mapping = {
-    "|": ((1,3), (1,), (1,3), (3,)),
-    "-": ((0,), (0,2), (2,), (0,2)),
-    "/":  ((3,), (2,), (1,), (0,)),
-    "\\": ((1,), (0,), (3,), (2,)),
-    ".":  ((0,), (1,), (2,), (3,)),
-}
-while len(q):
-    qq = []
-    for i, j, d in q:
-        if vis[i][j][d]: continue
-        vis[i][j][d] = True
-        for dd in mapping[lines[i][j]][d]:
-            di, dj = DIRS[dd]
-            ni = i+di
-            nj = j+dj
-            if 0 <= ni < n and 0 <= nj < m:
-                qq.append((ni, nj, dd))
-    q = qq
+def startfrom(INIT=(0, 0, 0)):
+    q = [INIT]
+    vis = [[[False,False,False,False] for w in line] for line in lines]
+    mapping = {
+        "|": ((1,3), (1,), (1,3), (3,)),
+        "-": ((0,), (0,2), (2,), (0,2)),
+        "/":  ((3,), (2,), (1,), (0,)),
+        "\\": ((1,), (0,), (3,), (2,)),
+        ".":  ((0,), (1,), (2,), (3,)),
+    }
+    while len(q):
+        qq = []
+        for i, j, d in q:
+            if vis[i][j][d]: continue
+            vis[i][j][d] = True
+            for dd in mapping[lines[i][j]][d]:
+                di, dj = DIRS[dd]
+                ni = i+di
+                nj = j+dj
+                if 0 <= ni < n and 0 <= nj < m:
+                    qq.append((ni, nj, dd))
+        q = qq
 
-# for line in vis:
-#     print("".join("#" if max(x) else "." for x in line))
+    # for line in vis:
+    #     print("".join("#" if max(x) else "." for x in line))
 
+    ans = 0
+    for line in vis:
+        for cell in line:
+            ok = False
+            for w in cell:
+                if w:
+                    ok = True
+            if ok: ans += 1
+    # print(ans)
+    return ans
+
+print(startfrom())
 ans = 0
-for line in vis:
-    for cell in line:
-        ok = False
-        for w in cell:
-            if w:
-                ok = True
-        if ok: ans += 1
+for i in range(n):
+    ans = max(ans, startfrom((i, 0, 0)))
+    ans = max(ans, startfrom((i, m-1, 2)))
+for j in range(m):
+    ans = max(ans, startfrom((0, j, 1)))
+    ans = max(ans, startfrom((n-1, j, 3)))
 print(ans)
