e70b266324d63c963bacd6240cfbe6be3f31e7eb
NovaSagittarii <novasagittarii@gmail.com>
2023-12-16 23:18:14 -0700

    d17 p2 1426
    well that was pretty simple, kinda funny to have so many dimensions tho

--- 2023/17/index.py
+++ 2023/17/index.py
@@ -16,31 +16,33 @@ m = len(lines[0])
 """
 DIR = ((0,1), (1,0), (0,-1), (-1,0))
 
-vis = [[[[False for _r in range(4)] for _d in range(4)] for _j in line] for line in lines]
-# cost, i, j, curdir, remaining
+MAXCONSEC = 10
+vis = [[[[False for _r in range(MAXCONSEC+1)] for _d in range(4)] for _j in line] for line in lines]
+# cost, i, j, curdir, consec
 import heapq
 pq = []
-def insert(c, i, j, dir, remaining):
+def insert(c, i, j, dir, consec):
     di, dj = DIR[dir]
     ni = i + di
     nj = j + dj
     if 0 <= ni < n and 0 <= nj < m:
-        heapq.heappush(pq, (c+lines[ni][nj], ni, nj, dir, remaining-1))
-for i in range(3): insert(0, 0, 0, i, 3)
+        heapq.heappush(pq, (c+lines[ni][nj], ni, nj, dir, consec+1))
+for i in range(4): insert(0, 0, 0, i, 0)
 
 while len(pq):
-    cost, i, j, curdir, remaining = heapq.heappop(pq)
+    cost, i, j, curdir, consec = heapq.heappop(pq)
     # go straight?
-    if vis[i][j][curdir][remaining]: continue
+    if vis[i][j][curdir][consec]: continue
     if i == n-1 and j == m-1:
         print(cost)
         break
-    vis[i][j][curdir][remaining] = True
-    if remaining > 0:
-        insert(cost, i, j, curdir, remaining)
+    vis[i][j][curdir][consec] = True
+    if consec < 10:
+        insert(cost, i, j, curdir, consec)
     # turn left/right
-    insert(cost, i, j, (curdir-1)%4, 3)
-    insert(cost, i, j, (curdir+1)%4, 3)
+    if consec >= 4:
+        insert(cost, i, j, (curdir-1)%4, 0)
+        insert(cost, i, j, (curdir+1)%4, 0)
 
 # for line in vis:
 #     print("".join('#' if x else '.' for x in line))
