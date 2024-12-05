9b5b03f7a2d8cfb1709e04eb71abe5b71a8c1a80
Thomas Li <tli186@ucr.edu>
2024-12-04 22:08:18 -0700

    d5 p2 131

--- 2024/05/main.py
+++ 2024/05/main.py
@@ -19,6 +19,7 @@ for line in lines:
         A.append(arrayint(line.split(',')))
 
 tot = 0
+tot2 = 0
 for a in A:
     ok = True
     n = len(a)
@@ -27,10 +28,13 @@ for a in A:
             for u, v in rule:
                 if a[i] == v and a[j] == u:
                     ok = False
-                    break
-            if not ok: break
-        if not ok: break
+                    a[i] = u
+                    a[j] = v
     if ok:
         # print(a, a[n//2])
         tot += a[n//2]
+    else:
+        # print(a, a[n//2])
+        tot2 += a[n//2]
 print(tot)
+print(tot2)
