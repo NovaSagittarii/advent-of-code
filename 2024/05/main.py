fa3170e6777015cc633c394e24dffe4ac1a2f243
Thomas Li <tli186@ucr.edu>
2024-12-04 22:07:20 -0700

    d5 p1 237

---
+++ 2024/05/main.py
@@ -0,0 +1,36 @@
+import math
+import re
+import sys
+lines = [line.strip() for line in sys.stdin]
+arrayint = lambda arr: list(int(x) for x in arr)
+
+# >v<^
+SLOPE = list(">v<^")
+DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+A = []
+rule = []
+for line in lines:
+    # A.append(arrayint(line.split()))
+    if not line: continue
+    if "|" in line:
+        rule.append(arrayint(line.split('|')))
+    else:
+        A.append(arrayint(line.split(',')))
+
+tot = 0
+for a in A:
+    ok = True
+    n = len(a)
+    for i in range(n):
+        for j in range(i+1, n):
+            for u, v in rule:
+                if a[i] == v and a[j] == u:
+                    ok = False
+                    break
+            if not ok: break
+        if not ok: break
+    if ok:
+        # print(a, a[n//2])
+        tot += a[n//2]
+print(tot)
