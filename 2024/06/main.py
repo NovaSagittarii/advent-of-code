8fc67b08b4aabef518f2edfa40d64ea258e5b9e6
Thomas Li <tli186@ucr.edu>
2024-12-06 00:01:52 -0700

    d6 p2 1375

--- 2024/06/main.py
+++ 2024/06/main.py
@@ -14,13 +14,16 @@ for line in lines:
 
 n = len(A)
 m = len(A[0])
-xi, xj = 0, 0
+si, sj = 0, 0
 for i in range(n):
     for j in range(m):
         if A[i][j] == '^':
-            xi, xj = i, j
+            si, sj = i, j
 xd = 3
 
+B = [a[:] for a in A]
+xi, xj, xd = si, sj, 3
+A = [a[:] for a in B]
 vis = set()
 while True:
     k = (xi, xj, xd)
@@ -48,3 +51,48 @@ tot = 0
 for l in A:
     tot += len([c for c in l if c == 'X'])
 print(tot)
+
+C = [a[:] for a in A]
+A = [a[:] for a in B]
+tot2 = 0
+for i in range(n):
+    for j in range(m):
+        if A[i][j] != '.': continue
+        if C[i][j] != 'X': continue
+        vis = set()
+        looped = False
+        xi, xj, xd = si, sj, 3
+        A[i][j] = '#'
+        # for w in A: print(w)
+        # print()
+        while True:
+            k = (xi, xj, xd)
+            if k in vis:
+                looped = True
+                break
+            vis.add(k)
+            di, dj = DIR[xd]
+            ni = xi + di
+            nj = xj + dj
+            while 0 <= ni < n and 0 <= nj < m and A[ni][nj] == '#':
+                xd = (xd + 1) % 4
+                di, dj = DIR[xd]
+                ni = xi + di
+                nj = xj + dj
+
+                k = (xi, xj, xd)
+                if k in vis:
+                    looped = True
+                    break
+                vis.add(k)
+            if 0 <= ni < n and 0 <= nj < m and A[ni][nj] != '#':
+                xi = ni
+                xj = nj
+            else:
+                break # looped!
+        if looped:
+            tot2 += 1
+            # for a in A: print(''.join(a))
+            # print()
+        A[i][j] = '.'
+print(tot2)
