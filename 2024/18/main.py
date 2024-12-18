617b20cb12c475370edbf4a7417ac256e2d003f0
Thomas Li <tli186@ucr.edu>
2024-12-17 22:30:35 -0700

    d18 p2 2143
    binary search my beloved

--- 2024/18/main.py
+++ 2024/18/main.py
@@ -14,34 +14,47 @@ for line in lines:
 
 n = 71
 T = len(A)
-a = [[0 for i in range(n)] for j in range(n)]
-for t, xy in enumerate(A[:1024]):
-    x, y = xy
-    a[y][x] = 1
 
-for row in a: print(*row)
+def doesitwork(W):
+    a = [[0 for i in range(n)] for j in range(n)]
+    for t, xy in enumerate(A[:W]):
+        x, y = xy
+        a[y][x] = 1
 
-q = [(0, 0, 0)]
-ans = 0
-vis = set()
-while len(q):
-    nq = []
-    ok = False
-    for t, i, j in q:
-        if a[i][j]: continue
-        # print(i, j, t, a[i][j])
-        k = (i, j)
-        if k in vis: continue
-        vis.add(k)
-        if i == n-1 and j == n-1:
-            print(t)
-            ok = True
-            break
-        for di, dj in DIR:
-            ni = i + di
-            nj = j + dj
-            if 0 <= ni < n and 0 <= nj < n:
-                nq.append((t+1, ni, nj))
-    if ok: break
-    q = nq
+    # for row in a: print(*row)
 
+    q = [(0, 0, 0)]
+    ans = 0
+    vis = set()
+    while len(q):
+        nq = []
+        ok = False
+        for t, i, j in q:
+            if a[i][j]: continue
+            # print(i, j, t, a[i][j])
+            k = (i, j)
+            if k in vis: continue
+            vis.add(k)
+            if i == n-1 and j == n-1:
+                return t
+            for di, dj in DIR:
+                ni = i + di
+                nj = j + dj
+                if 0 <= ni < n and 0 <= nj < n:
+                    nq.append((t+1, ni, nj))
+        if ok: break
+        q = nq
+    return -1
+
+print(doesitwork(1024))
+lo = 0
+hi = T
+lmid = -1
+while lo <= hi:
+    mid = lo + (hi - lo) // 2
+    if doesitwork(mid) >= 0:
+        lo = mid + 1
+        lmid = mid
+    else:
+        hi = mid - 1
+print(A[lmid])
