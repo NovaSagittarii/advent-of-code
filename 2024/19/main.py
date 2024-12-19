a58239d0d57902686f37283fc9b0e242c939586b
Thomas Li <tli186@ucr.edu>
2024-12-18 22:08:51 -0700

    d19 p1 711

---
+++ 2024/19/main.py
@@ -0,0 +1,34 @@
+import math
+import re
+import sys
+lines = [line.strip() for line in sys.stdin]
+arrayint = lambda arr: list(int(x) for x in arr)
+
+# >v<^
+SLOPE = list(">v<^")
+DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+A = lines[0].split(', ')
+Q = lines[2:]
+
+ans = 0
+for q in Q:
+    n = len(q)
+    dp = [0 for i in range(n+1)]
+    dp[0] = 1
+    for i in range(n):
+        if not dp[i]: continue
+        for a in A:
+            if i+len(a) > n: continue
+            ok = True
+            for j, c in enumerate(list(a)):
+                if q[i+j] != c:
+                    ok = False
+                    break
+            if ok:
+                dp[i + len(a)] = 1
+    print(q, dp[-1])
+    # print(dp)
+    if dp[-1]:
+        ans += 1
+print(ans)
