48576099b7a78361a6a817d6626695f10474fcb3
Thomas Li <tli186@ucr.edu>
2024-12-24 00:16:50 -0700

    d24 p1 134
    eda be like

--- 2024/24/main.py
+++ 2024/24/main.py
@@ -8,7 +8,31 @@ arrayint = lambda arr: list(int(x) for x in arr)
 SLOPE = list(">v<^")
 DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-A = []
+dp = {}
+defs = {}
 for line in lines:
-    A.append(list(line))
+    if ':' in line:
+        k, v = line.split(': ')
+        v = int(v)
+        dp[k] = v
+    elif '->' in line:
+        xy, z = line.split(' -> ')
+        defs[z] = tuple(xy.split())
+def dfs(u):
+    if u in dp: return dp[u]
+    x, op, y = defs[u]
+    x = dfs(x)
+    y = dfs(y)
+    if op == 'OR': ret = x | y
+    elif op == 'AND': ret = x & y
+    elif op == 'XOR': ret = x ^ y
+    dp[u] = ret
+    return ret
 
+for u in defs.keys(): dfs(u)
+ans = ""
+for u in sorted(defs.keys(), reverse=True):
+    if u[0] == 'z':
+        # print(u, dfs(u))
+        ans += str(dfs(u))
+print(int(ans, 2))
