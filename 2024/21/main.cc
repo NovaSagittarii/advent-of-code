18f120c02c0c0c1f071af9b7d86f18ac79ececb1
Thomas Li <tli186@ucr.edu>
2024-12-24 21:59:13 -0700

    d21 p2 13274
    that was also tough

--- 2024/21/main.cc
+++ 2024/21/main.cc
@@ -1,49 +1,4 @@
 /*
-
-IN
-
-6A
-836A
-540A
-965A
-480A
-789A
-
-
-OUT
-
-836A
-836A: 30
-540A
-540A: 30
-965A
-965A: 28
-480A
-480A: 30
-789A
-789A: 28
-104792 -- 1
-
-836A
-836A: 70
-540A
-540A: 72
-965A
-965A: 66
-480A
-480A: 74
-789A
-789A: 66
-248684 -- 2
-
-8: 57386004
-
-30 70 170 426 1050 2618
-30 72 184 446 1120 2772
-28 66 160 398 982 2442
-30 74 182 444 1106 2734
-28 66 164 396 990 2436
-
 err suppose you found a best sequence 
 can you just extend this sequence? to get a new best sequence??
 
@@ -100,6 +55,9 @@ typedef long long ll;
 typedef std::vector<int> vi;
 typedef std::vector<vi> v2i;
 typedef std::vector<v2i> v3i;
+typedef std::vector<ll> vll;
+typedef std::vector<vll> v2ll;
+typedef std::vector<v2ll> v3ll;
 #define setmax(a, b) a = std::max(a, b);
 #define setmin(a, b) a = std::min(a, b);
 
@@ -120,6 +78,56 @@ int main() {
     // int n;
     // std::cin >> n;
     
+    v3ll dp(26, v2ll(5, vll(5, -1)));
+    std::vector<std::vector<std::array<int, 2>>> dadj = {
+      {{1, 4}, {3, 3}}, // 0 (^)
+      {{0, 2}, {4, 3}}, // 1 (A)
+      {{3, 4}}, // 2 (<)
+      {{0, 0}, {2, 2}, {4, 4}}, // 3 (v)
+      {{1, 0}, {3, 2}}, // 4 (>)
+    };
+    auto dfs = [&](int lv, int s, int t, auto&& dfs) -> ll {
+      // std::cout << lv << " " << s << " " << t << std::endl;
+      if (lv == 0) return 1;
+      if (s == t) return 1; // smack A
+      if (dp[lv][s][t] != -1) return dp[lv][s][t];
+      ll tot = 1e18;
+      std::priority_queue<std::array<ll, 3>> pq;
+      // ll tA = dfs(lv-1, t, 1, dfs); // gotta press A at the end
+      pq.push({0, s, 1}); // {cost, curr_lv, lower_level}
+      while (!pq.empty()) {
+        auto [c, u, ppos] = pq.top();
+        pq.pop();
+        c *= -1;
+        // if (lv == 2) std::cout << c << " " << u << " " << ppos << std::endl;
+        if (u == t) {
+          if (ppos == 1) {
+            tot = c;
+            break;
+          } else {
+            ll C = c + dfs(lv-1, ppos, 1, dfs);
+            pq.push({-C, u, 1});
+          }
+        }
+        for (auto [v, pos] : dadj[u]) {
+          ll C = c + dfs(lv-1, ppos, pos, dfs);
+          pq.push({-C, v, pos});
+        }
+      }
+      return dp[lv][s][t] = tot;
+    };
+
+    // std::cout << dfs(1, 1, 2, dfs) << std::endl; // 4
+    // std::cout << dfs(2, 1, 2, dfs) << std::endl; // 10
+    // std::cout << dfs(1, 2, 1, dfs) << std::endl; // 4
+    // std::cout << dfs(2, 2, 1, dfs) << std::endl; // 8
+    // std::cout << dfs(2, 3, 1, dfs) << std::endl; // 7
+    // std::cout << dfs(1, 4, 1, dfs) << std::endl; // 2
+    // std::cout << dfs(2, 4, 1, dfs) << std::endl; // 4
+    // std::cout << dfs(2, 1, 1, dfs) << std::endl; // 1
+    // std::cout << dfs(2, 2, 2, dfs) << std::endl; // 1
+    // continue;
+
     std::string s;
     ll ans = 0;
     while (std::cin >> s) {
@@ -134,6 +142,13 @@ int main() {
         ".^A",
         "<v>",
       };
+      std::map<char, int> dpad = {
+        {'^', 0},
+        {'A', 1},
+        {'<', 2},
+        {'v', 3},
+        {'>', 4},
+      };
       std::map<char, std::array<int, 2>> mdij = {
         {'^', {-1, 0}},
         {'>', {0, 1}},
@@ -141,8 +156,8 @@ int main() {
         {'<', {0, -1}},
       };
 
-      const int howmuchpain = 1;
-      // const int howmuchpain = 8;
+      // const int howmuchpain = 2;
+      const int howmuchpain = 25;
       
       typedef std::tuple<int, std::array<int, 2>, std::string> node;
       std::queue<node> q;
@@ -158,6 +173,7 @@ int main() {
           if (i < 0 || i >= 4 || j < 0 || j >= 3 || a[i][j] == '.') continue;
           if (a[i][j] == s[prog]) {
             ++prog; // yay a match
+            u.push_back('A');
             // could be a while but no repeated in input
           }
           if (prog == s.size()) {
@@ -167,39 +183,29 @@ int main() {
             // break;
           }
           // try moving a bit
-          for (int d = 0; d < 4; ++d) {
-            auto [di, dj] = dij[d];
-            auto dirs = u;
-            ++dirs[d];
-            q.push({prog, {i + di, j + dj}, dirs});
+          for (auto [c, didj] : mdij) {
+            auto [di, dj] = didj;
+            auto uu = u;
+            uu.push_back(c);
+            q.push({prog, {i + di, j + dj}, uu});
           }
         }
       }
-      auto u = candidate[0];
-      for (auto x : u) std::cout << x << " ";
-      std::cout << std::endl;
-      for (int i = 0; i < howmuchpain; ++i){
-        /**
-         *              ^ > v < A
-         * ^ ~ <A>A     0 1 0 1 2
-         * > ~ vA^A     1 0 1 0 2
-         * v ~ <vA^>A   1 1 1 1 2
-         * < ~ <<vA^>>A 1 2 1 2 2
-         * A ~ A        0 0 0 0 1
-         */
-        auto [w, d, s, a, A] = u;
-        a5int v = {
-          d + s + a,
-          w + s + a*2,
-          d + s + a,
-          w + s + a*2,
-          1*(w+s+a+d) + A,
-        };
-        u = v;
+
+      ll tot = 1e18;
+      for (auto str : candidate) {
+        ll res = 0;
+        int prev = dpad['A'];
+        for (auto c : str) {
+          res += dfs(howmuchpain, prev, dpad[c], dfs);
+          prev = dpad[c];
+        }
+        // std::cout << str << " " << res << "\n";
+        setmin(tot, res);
       }
 
-      ll tot = 0;
-      for (auto x : u) tot += x;
+
+      // for (auto x : u) tot += x;
       std::cout << s << ": " << tot << std::endl;
       std::istringstream in(s);
       int w;
