667f656a49af7a263d55c541a2a240517d2e7544
Thomas Li <tli186@ucr.edu>
2024-12-24 22:08:41 -0700

    d25 374 316
    well that was kinda a chill puzzle to end the year

--- 2024/25/main.py
+++ 2024/25/main.py
@@ -8,7 +8,28 @@ arrayint = lambda arr: list(int(x) for x in arr)
 SLOPE = list(">v<^")
 DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-A = []
-for line in lines:
-    A.append(list(line))
+keys = []
+locks = []
+for k in range(0, len(lines), 8):
+    lo = [9 for _ in range(5)]
+    hi = [0 for _ in range(5)]
+    for i in range(7):
+        for j, c in enumerate(lines[k+i]):
+            if c == '#': lo[j] = min(lo[j], i)
+            if c == '#': hi[j] = max(hi[j], i)
+    if lines[k][0] == '#': # a lock
+        locks.append(hi)
+    else:
+        keys.append(lo)
 
+tot = 0
+for key in keys:
+    for lock in locks:
+        ok = True
+        for i in range(5):
+            if key[i] <= lock[i]:
+                ok = False
+        if ok:
+            tot += 1
+            # print(key, lock)
+print(tot)
