bb99c9c04acc333a8e47fccb24ea9a0cedf7d3f6
Thomas Li <tli186@ucr.edu>
2025-12-02 22:12:43 -0700

    2025 03 p2
    took a while to think about it oop, it's nice there's a short greedy solution though

--- 2025/03/main.py
+++ 2025/03/main.py
@@ -9,11 +9,23 @@ SLOPE = list(">v<^")
 DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 ans = 0
+ans2 = 0
 for line in lines:
-    best = 0
     n = len(line)
-    for i in range(n):
-        for j in range(i+1, n):
-            best = max(best, int(line[i] + line[j]))
-    ans += best
+    def calc(k):
+        cur = -1
+        res = []
+        for i in reversed(range(k)):
+            for j in range(cur, n-i):
+                if cur < 0 or line[j] > line[cur]:
+                    cur = j
+            res.append(line[cur])
+            cur += 1
+        # print(int("".join(res)))
+        return int("".join(res))
+    ans += calc(2)
+    ans2 += calc(12)
 print(ans)
+print(ans2)
+# assert ans == 357
+# assert ans2 == 3121910778619
