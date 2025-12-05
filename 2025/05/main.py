1af19a64c0c20546d5d78be3258549e415a2df4d
Thomas Li <novasagittarii@gmail.com>
2025-12-04 21:12:54 -0800

    2025 05 p2
    oops i forgot to handle negative sized intervals,
    but i guess it wasn't too bad.
    
    i dislike this workspace though...
    and i miss autoindent :pensive:

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -27,3 +27,21 @@ for x in B:
             ans += 1
             break
 print(ans)
+
+lrs = []
+for a in A:
+    l, r = map(int, a.split('-'))
+    if l > r: print(f"{l}\n{r}\n")
+    assert l <= r
+    lrs.append((l, r+1))
+lrs.sort()
+
+ans2 = 0
+last = 0
+for l, r in lrs:
+    l = max(l, last)
+    last = max(last, r)
+    # print(l, r)
+    if r > l: ans2 += r - l
+assert ans2 != 325650347334979 # err
+print(ans2)
