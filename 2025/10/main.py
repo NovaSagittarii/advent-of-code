178235926ad89a84dbcc5ca0a63a73b179d31240
Thomas Li <tli186@ucr.edu>
2025-12-09 22:37:41 -0700

    2025 10 p2
    LP go brrr, thanks linear optimization solvers
    
    for some reason i initially thought that it was get to the
    level or higher... :^)

--- 2025/10/main.py
+++ 2025/10/main.py
@@ -1,19 +1,20 @@
-import math
-import re
 import sys
+import numpy as np
+import scipy.optimize
 
 lines = [line.strip() for line in sys.stdin]
-arrayint = lambda arr: list(int(x) for x in arr)
 
 # >v<^
 SLOPE = list(">v<^")
 DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 tot = 0
+tot2 = 0
 for line in lines:
     a, *choice, b = line.split()
     n = len(a) - 2
     a = sum(1 << i for i, x in enumerate(a[1:-1]) if x == "#")
+    req = tuple(map(int, b[1:-1].split(',')))
 
     def parse(a):
         return sum(1 << int(x) for x in a[1:-1].split(","))
@@ -36,4 +37,21 @@ for line in lines:
             best = min(best, cost)
     # print(best)
     tot += best
+
+    c = np.ones((k))
+    b_eq = np.zeros((n))
+    A_eq = np.zeros((n, k))
+    for i, x in enumerate(choice):
+        for j in range(n):
+            if x & (1 << j):
+                A_eq[j][i] = 1
+    for i, x in enumerate(req):
+        b_eq[i] = x
+    res = scipy.optimize.linprog(c, A_eq=A_eq, b_eq=b_eq, integrality=1)
+    assert res.success
+    tot2 += round(res.fun)
+    # print(res)
+
 print(tot)
+print(tot2)
+assert tot2 != 15249, "WA"
