01f0eb88b9a29f2d268743c725b7378a81912b98
Thomas Li <tli186@ucr.edu>
2025-12-11 22:19:47 -0700

    2025 12 p1
    heuristic guessing go brrr

---
+++ 2025/12/main.py
@@ -0,0 +1,29 @@
+import math
+import re
+import sys
+
+lines = [line.strip() for line in sys.stdin]
+arrayint = lambda arr: list(int(x) for x in arr)
+
+# >v<^
+SLOPE = list(">v<^")
+DIR = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+*shapes, queries = ("\n".join(lines)).split("\n\n")
+
+ans = 0
+for q in queries.split("\n"):
+    sz, *order = q.split()
+    n, m = map(int, sz[:-1].split("x"))
+    order = tuple(map(int, order))
+    # print(n * m, sum(order) * 8)
+    wei = [5, 7, 6, 8, 8, 7]  # hardcoded lol (how many # in a block)
+    req = sum(x * y for x, y in zip(order, wei))
+    if req < n * m:
+        ans += 1
+    # print(n * m - req)
+print(ans)
+# a = []
+# for line in lines:
+#     a.append(tuple(map(int, line.split(","))))
+# n = len(a)
